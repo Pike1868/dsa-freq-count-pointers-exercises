e39629381b63d9ec2471f9b14cf290ad14383b72
Pike1868 <luis_14mendoza@protonmail.com>
2024-03-05 13:33:53 -0500

    Added my solutions to longestFall and pivotIndex problems

--- longest-fall.js
+++ longest-fall.js
@@ -1,3 +1,31 @@
+/**longestFall
+ * Write a function called longestFall,
+ * which accepts an array of integers,
+ * and returns the length of the longest
+ * consecutive decrease  of integers.
+ *
+ * ex:
+ * longestFall([5,3,1,3,0]) // 3,  5-3-1 is the longest consecutive sequence of decreasing integers
+ */
+
 // add whatever parameters you deem necessary
-function longestFall() {
+function longestFall(arr) {
+  //handle simple edge cases
+  if (!arr.length) return 0;
+
+  //initialize count and maxLength
+  let count = 1;
+  let maxLength = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i - 1] > arr[i]) {
+      count++;
+      maxLength = Math.max(count, maxLength);
+    } else {
+      count = 1;
+    }
+  }
+  return maxLength || 1;
 }
+
+module.exports = longestFall;
--- pivot-index.js
+++ pivot-index.js
@@ -1,3 +1,24 @@
+/**pivotIndex
+ * Write a function called pivotIndex,
+ * which accepts an array of integers,
+ * and returns the pivot index where
+ * the sum of the items to the left equal
+ * to the sum of the items to the right
+ */
 // add whatever parameters you deem necessary
-function pivotIndex() {
+function pivotIndex(arr) {
+  let rightSum = arr.reduce((a, b) => a + b, 0);
+  let leftSum = 0;
+  let pivotIdx = -1;
+  for (let i = 0; i < arr.length; i++) {
+    leftSum += arr[i];
+    if (leftSum === rightSum) {
+      pivotIdx = i;
+      break;
+    }
+    rightSum -= arr[i];
+  }
+  return pivotIdx;
 }
+
+module.exports = pivotIndex;
